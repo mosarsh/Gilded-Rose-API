bd87227919dc7bcefaa25be76e3da661b3836cfd
mosarsh <mosarsh@yahoo.com>
2019-05-20 17:33:09 -0400

    merge

--- GildedRose.Api/App_Start/WebApiConfig.cs
+++ GildedRose.Api/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace GildedRose.Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "v1/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
