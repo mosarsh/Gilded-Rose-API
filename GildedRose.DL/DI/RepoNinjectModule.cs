19304820b85498d7e0a55b56d9f3adb6ee827f0b
mosarsh <mosarsh@yahoo.com>
2019-05-20 13:40:04 -0400

    DI Setup for Repoitory

---
+++ GildedRose.DL/DI/RepoNinjectModule.cs
@@ -0,0 +1,20 @@
+﻿using GildedRose.DL.Repository;
+using GildedRose.DL.UoW;
+using Ninject.Modules;
+
+namespace GildedRose.DL.DI
+{
+    public class RepoNinjectModule : NinjectModule
+    {
+        /// <summary>
+        /// Overriding load method to register DI
+        /// </summary>
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+            Bind<IInventoryRepository>().To<InventoryRepository>();
+            Bind<IPurchaseInventoryRepository>().To<PurchaseInventoryRepository>();
+            Bind<IUserRepository>().To<UserRepository>();
+        }
+    }
+}
